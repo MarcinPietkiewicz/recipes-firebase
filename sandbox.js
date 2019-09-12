bb0a1cfbc9fb175786a1684b94948dcf6d8f3fd9
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2019-09-12 22:40:52 +0200

    Added delete functionlity to recipe posts

--- sandbox.js
+++ sandbox.js
@@ -1,43 +1,54 @@
 const form = document.querySelector('form');
 const list = document.querySelector('ul');
 
-const addRecipe = recipe => {
+// adding recipe to DOM
+
+const addRecipe = (recipe, id) => {
     let time = recipe.created_at.toDate();
     let html = `
-    <li>
-    <div>${recipe.title} 
-    <div style="font-size:small; color:#999;"> ${time}</div>
-</div>
+    <li data-id="${id}">
+    <div>${recipe.title} </div>
+    <div style="font-size:small; color:#999;">${time} </div>
+    <button class="btn btn-danger btn-sm my-2">delete</button>
     </li>
     `;
     list.innerHTML += html;
 }
 
-
+// fetching recipes
 db.collection('recipes').get().then((snapshot) => {
+    // when we have the data 
     snapshot.forEach(doc => {
-        addRecipe(doc.data());
+        addRecipe(doc.data(), doc.id);
     })
 }).catch(err => {
     console.log(err)
 })
 
-
-// add docuemnts
-
+// submitting recipe to firestore
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     const now = new Date();
-
     const recipe = {
         title: form.recipe.value,
         created_at: firebase.firestore.Timestamp.fromDate(now),
-
     };
 
     db.collection('recipes').add(recipe).then(() => {
         console.log('recipe added')
     }).catch(err => { console.log(err) });
+
+})
+
+// deleting recipe
+list.addEventListener('click', e => {
+    if (e.target.tagName === 'BUTTON') {
+        const id = e.target.parentElement.getAttribute('data-id')
+        db.collection('recipes').doc(id).delete().then(() => {
+            console.log('recipe deleted');
+        });
+    }
+
 
 })
